80ca1b06c646c7018bac87e05e5ac8b2a0466cf0
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2012-09-23 11:31:22 +0200

    works

---
+++ unfollow.pl
@@ -0,0 +1,75 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Net::Twitter::Lite;
+use Try::Tiny;
+use YAML;
+
+
+my $cfg= YAML::LoadFile("unfollow.yml");
+ 
+my $nt=Net::Twitter::Lite->new(
+    legacy_lists_api => 0,
+    consumer_key        =>$cfg->{consumer_key},
+    consumer_secret     =>$cfg->{consumer_secret},
+    access_token        =>$cfg->{access_token},
+    access_token_secret =>$cfg->{access_token_secret},
+
+);
+my $id=$cfg->{user_id};
+my $count=$cfg->{unfollow_count}; 
+
+
+#from http://tips.kaali.co.uk/2011/05/01/how-to-bulk-unfollow-on-twitter-using-perl/
+ 
+
+my $cnt=0;
+my $followers="";
+my @friends=();
+
+use Data::Dumper;
+
+#-- Fetch followers
+try {
+    my $follow=$nt->followers_ids($id);
+    my @fids= @{$follow->{ids}};
+    $followers=join(' ',@fids);
+      
+} catch {
+    die @_;
+};
+
+if($@){print "\n\n$@\n"; exit 1; }
+ 
+#-- Fetch friends
+try {
+    
+    my $f= $nt->friends_ids($id);
+    @friends=@{$f->{ids}};
+}
+catch {
+    die @_;
+};
+
+if($@){print "\n\n$@\n";exit 2;}
+ 
+#-- Now find which of the friends does not follow you
+for my $friend (@friends){
+ if(index($followers,$friend)>=0){
+ print "\n$friend follows $id";
+ }
+ else{
+ #-- At this point, Unfollow the Friend if you like
+ eval{
+ my $fname=$nt->destroy_friend($friend);
+ $cnt+=1;
+ print "\n$cnt. Unfollowing $fname->{screen_name}...";
+ };
+ print "\n$@" if $@;
+ last if $cnt==$count;
+ }
+}
+ 
+print "\n\n";
+ 
+1;
